0e6f6754b2415be30cd6dff14a6a18d3e5ef860d
Alã Pedro <98903879+AlaPedro@users.noreply.github.com>
2025-03-07 01:59:22 +0000

    feat/ criando base para subir na vercel

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+function Home () {
+    return (
+    <div>
+        <h1>
+            Jompa!
+        </h1>
+        <br/>
+        <span>
+            Você é a pessoa escolhida pra receber essa mensagem requisitada pelo Deschamps! haha <br/>
+            muito obrigado por todos os ensinamentos e direcionamento que tem me dado até os dias de hoje. <br/>
+            <strong>te amo!</strong>
+        </span>
+        <br/>
+        <h1>
+            Com carinho, Badala
+        </h1>
+        <br/>
+        <p>ps: é muito bom escrever com html haha, estava com saudades</p>
+    </div>
+    )
+}
+
+export default Home;
